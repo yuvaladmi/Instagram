8d421862e7a086614ab22d979846c0b47437717b
Omer.R. <omer.rots@gmail.com>
2025-04-08 09:51:29 +0300

    add profile grid

--- src/cmps/StoriesList.jsx
+++ src/cmps/StoriesList.jsx
@@ -1,47 +1,67 @@
-import { StoryPreview } from "./StoryPreview.jsx"
-import { useEffect } from 'react'
-import { useSelector } from 'react-redux'
-import { loadStories, addComment, addLike } from '../store/actions/story.actions.js'
-import { loadUsers, savedStoryUser } from  '../store/actions/user.actions.js';
+import { StoryPreview } from "./StoryPreview.jsx";
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
+import {
+	loadStories,
+	addComment,
+	addLike,
+} from "../store/actions/story.actions.js";
+import { loadUsers, savedStoryUser } from "../store/actions/user.actions.js";
 
 export function StoriesList() {
-//TODO - move to StoriesIndex
-    const stories = useSelector(storeState => storeState.storyModule.stories)
-    const currentUser = useSelector(storeState => storeState.userModule.loggedInUser)
+	//TODO - move to StoriesIndex
+	const stories = useSelector(storeState => storeState.storyModule.stories);
+	const currentUser = useSelector(
+		storeState => storeState.userModule.loggedInUser
+	);
 
-    useEffect(() => {
-        if (stories.length && currentUser) return
-        loadStories();
-        loadUsers();
-    }, [])
+	useEffect(() => {
+		if (stories.length && currentUser) return;
+		loadStories();
+		loadUsers();
+	}, []);
 
-    function onSetNewComment(storyId, newComment){
-        addComment(newComment, storyId, currentUser);
-    }
-    function onNewLike(storyId){
-        addLike(storyId, currentUser);
-    }
+	function onSetNewComment(storyId, newComment) {
+		addComment(newComment, storyId, currentUser);
+	}
+	function onNewLike(storyId) {
+		addLike(storyId, currentUser);
+	}
 
-    function onSaveStory(storyId){
-        savedStoryUser(storyId, currentUser._id)
-    }
-    
-    if (!stories.length && !currentUser) return <div className="loading-page"><span className="loading"></span></div>
-    return (
-        <div className="test">
-        <ul className="stories-list">
-            {stories.map(story =>
-                <li key={story._id}>
-                    <StoryPreview   story={story} 
-                                    onSetNewComment={onSetNewComment}
-                                    onNewLike={onNewLike}
-                                    onSaveStory={onSaveStory}
-                                    isLikeByUser={story.likedBy.some(user => {return user._id === currentUser._id})}
-                                    isSavedByUser={currentUser.savedStoryIds ? currentUser.savedStoryIds.some(savedStory => {return savedStory === story._id}) : false}
-                                    />
-                </li>
-            )}
-        </ul>
-        </div>
-    )
+	function onSaveStory(storyId) {
+		savedStoryUser(storyId, currentUser._id);
+	}
+
+	if (!stories.length && !currentUser)
+		return (
+			<div className="loading-page">
+				<span className="loading"></span>
+			</div>
+		);
+	return (
+		<div className="test">
+			<ul className="stories-list">
+				{stories.map(story => (
+					<li key={story._id}>
+						<StoryPreview
+							story={story}
+							onSetNewComment={onSetNewComment}
+							onNewLike={onNewLike}
+							onSaveStory={onSaveStory}
+							isLikeByUser={story.likedBy.some(user => {
+								return user._id === currentUser._id;
+							})}
+							isSavedByUser={
+								currentUser.savedStoryIds
+									? currentUser.savedStoryIds.some(savedStory => {
+											return savedStory === story._id;
+									  })
+									: false
+							}
+						/>
+					</li>
+				))}
+			</ul>
+		</div>
+	);
 }
--- src/services/story.service.js
+++ src/services/story.service.js
@@ -4,16 +4,16 @@ import { userService } from "./user.service.js";
 
 const STORAGE_KEY = "story";
 export const storyService = {
-  query,
-  getById,
-  save,
-  remove,
-  getEmptyStory,
-  onRemoveStoryComment,
-  createComment,
-  getDefaultFilter
-}
-window.ss = storyService
+	query,
+	getById,
+	save,
+	remove,
+	getEmptyStory,
+	onRemoveStoryComment,
+	createComment,
+	getDefaultFilter,
+};
+window.ss = storyService;
 
 function onRemoveStoryComment(storyId) {
 	return storageService.remove(STORAGE_KEY, storyId);
@@ -84,227 +84,593 @@ function getEmptyStory() {
 }
 
 function _createSrories() {
-  const story = [{
-        _id: "s101",
-        txt: "Best trip ever",
-        imgUrl: "https://res.cloudinary.com/djprda7rj/image/upload/v1722421161/foodPics/leizynqlraftxdsvmem0.jpg", 
-        by: {
-          _id: "u101",
-          fullname: "Ulash Ulashi",
-          imgUrl: "http://some-img"
-        },
-        loc: { // Optional
-          lat: 11.11, 
-          lng: 22.22,
-          name: "Tel Aviv"
-        },
-        comments: [
-          {
-            id: "c1001",
-            by: {
-              _id: "u105",
-              fullname: "Bob",
-              imgUrl: "http://some-img"
-            },
-            txt: "good one!",
-            likedBy: [ // Optional
-              {
-                "_id": "u105",
-                "fullname": "Bob",
-                "imgUrl": "http://some-img"
-              }
-            ]
-          },
-          {
-            id: "c1002",
-            by: {
-              _id: "u106",
-              fullname: "Dob",
-              imgUrl: "http://some-img"
-            },
-            txt: "not good!",
-          }
-        ],
-        likedBy: [
-          {
-            _id: "u105",
-            fullname: "Bob",
-            imgUrl: "http://some-img"
-          },
-          {
-            _id: "u106",
-            fullname: "Dob",
-            imgUrl: "http://some-img"
-          }
-        ],
-        tags: ["fun", "romantic"]
-      },
-      {
-        _id: "s101",
-        txt: "Best trip ever",
-        imgUrl: "https://cdn.pixabay.com/photo/2023/01/04/13/21/animals-7696695_640.jpg", 
-        by: {
-          _id: "u101",
-          fullname: "Ulash Ulashi",
-          imgUrl: "http://some-img"
-        },
-        loc: { // Optional
-          lat: 11.11, 
-          lng: 22.22,
-          name: "Tel Aviv"
-        },
-        comments: [
-          {
-            id: "c1001",
-            by: {
-              _id: "u105",
-              fullname: "Bob",
-              imgUrl: "http://some-img"
-            },
-            txt: "good one!",
-            likedBy: [ // Optional
-              {
-                "_id": "u105",
-                "fullname": "Bob",
-                "imgUrl": "http://some-img"
-              }
-            ]
-          },
-          {
-            id: "c1002",
-            by: {
-              _id: "u106",
-              fullname: "Dob",
-              imgUrl: "http://some-img"
-            },
-            txt: "not good!",
-          }
-        ],
-        likedBy: [
-          {
-            _id: "u105",
-            fullname: "Bob",
-            imgUrl: "http://some-img"
-          },
-          {
-            _id: "u106",
-            fullname: "Dob",
-            imgUrl: "http://some-img"
-          }
-        ],
-        tags: ["fun", "romantic"]
-      }  ,
-      {
-        _id: "s101",
-        txt: "Best trip ever",
-        imgUrl: "https://cdn.pixabay.com/photo/2023/01/04/13/21/animals-7696695_640.jpg", 
-        by: {
-          _id: "u101",
-          fullname: "Ulash Ulashi",
-          imgUrl: "http://some-img"
-        },
-        loc: { // Optional
-          lat: 11.11, 
-          lng: 22.22,
-          name: "Tel Aviv"
-        },
-        comments: [
-          {
-            id: "c1001",
-            by: {
-              _id: "u105",
-              fullname: "Bob",
-              imgUrl: "http://some-img"
-            },
-            txt: "good one!",
-            likedBy: [ // Optional
-              {
-                "_id": "u105",
-                "fullname": "Bob",
-                "imgUrl": "http://some-img"
-              }
-            ]
-          },
-          {
-            id: "c1002",
-            by: {
-              _id: "u106",
-              fullname: "Dob",
-              imgUrl: "http://some-img"
-            },
-            txt: "not good!",
-          }
-        ],
-        likedBy: [
-          {
-            _id: "u105",
-            fullname: "Bob",
-            imgUrl: "http://some-img"
-          },
-          {
-            _id: "u106",
-            fullname: "Dob",
-            imgUrl: "http://some-img"
-          }
-        ],
-        tags: ["fun", "romantic"]
-      }  ,
-      {
-        _id: "s101",
-        txt: "Best trip ever",
-        imgUrl: "https://cdn.pixabay.com/photo/2023/01/04/13/21/animals-7696695_640.jpg", 
-        by: {
-          _id: "u101",
-          fullname: "Ulash Ulashi",
-          imgUrl: "http://some-img"
-        },
-        loc: { // Optional
-          lat: 11.11, 
-          lng: 22.22,
-          name: "Tel Aviv"
-        },
-        comments: [
-          {
-            id: "c1001",
-            by: {
-              _id: "u105",
-              fullname: "Bob",
-              imgUrl: "http://some-img"
-            },
-            txt: "good one!",
-            likedBy: [ // Optional
-              {
-                "_id": "u105",
-                "fullname": "Bob",
-                "imgUrl": "http://some-img"
-              }
-            ]
-          },
-          {
-            id: "c1002",
-            by: {
-              _id: "u106",
-              fullname: "Dob",
-              imgUrl: "http://some-img"
-            },
-            txt: "not good!",
-          }
-        ],
-        likedBy: [
-          {
-            _id: "u105",
-            fullname: "Bob",
-            imgUrl: "http://some-img"
-          },
-          {
-            _id: "u106",
-            fullname: "Dob",
-            imgUrl: "http://some-img"
-          }
-        ],
-        tags: ["fun", "romantic"]
-      }          
-  ]
+	const story = [
+		{
+			_id: "s101",
+			txt: "Best trip ever",
+			imgUrl:
+				"https://res.cloudinary.com/djprda7rj/image/upload/v1722421161/foodPics/leizynqlraftxdsvmem0.jpg",
+			by: {
+				_id: "Xma2l",
+				fullname: "Koko Jambo",
+				imgUrl: "https://randomuser.me/api/portraits/women/16.jpg",
+			},
+			loc: {
+				// Optional
+				lat: 11.11,
+				lng: 22.22,
+				name: "Tel Aviv",
+			},
+			comments: [
+				{
+					id: "c1001",
+					by: {
+						_id: "u105",
+						fullname: "Bob",
+						imgUrl: "http://some-img",
+					},
+					txt: "good one!",
+					likedBy: [
+						// Optional
+						{
+							_id: "u105",
+							fullname: "Bob",
+							imgUrl: "http://some-img",
+						},
+					],
+				},
+				{
+					id: "c1002",
+					by: {
+						_id: "u106",
+						fullname: "Dob",
+						imgUrl: "http://some-img",
+					},
+					txt: "not good!",
+				},
+			],
+			likedBy: [
+				{
+					_id: "u105",
+					fullname: "Bob",
+					imgUrl: "http://some-img",
+				},
+				{
+					_id: "u106",
+					fullname: "Dob",
+					imgUrl: "http://some-img",
+				},
+			],
+			tags: ["fun", "romantic"],
+		},
+		{
+			_id: "s102",
+			txt: "wonderfull view",
+			imgUrl:
+				"https://plus.unsplash.com/premium_photo-1666792561369-88127aa152ed?w=468&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NjV8fHZpZXd8ZW58MHx8MHx8fDA%3D",
+			by: {
+				_id: "Xma2l",
+				fullname: "Koko Jambo",
+				imgUrl: "https://randomuser.me/api/portraits/women/16.jpg",
+			},
+			loc: {
+				// Optional
+				lat: 11.11,
+				lng: 22.22,
+				name: "Tel Aviv",
+			},
+			comments: [
+				{
+					id: "c1001",
+					by: {
+						_id: "u104",
+						fullname: "Bob",
+						imgUrl: "https://randomuser.me/api/portraits/men/32.jpg",
+					},
+					txt: "good one!",
+					likedBy: [
+						// Optional
+						{
+							_id: "u107",
+							fullname: "Joyce Lane",
+							imgUrl: "https://randomuser.me/api/portraits/women/3.jpg",
+						},
+					],
+				},
+				{
+					id: "c1002",
+					by: {
+						_id: "u106",
+						fullname: "Max Steward",
+						imgUrl: "https://randomuser.me/api/portraits/men/54.jpg",
+					},
+					txt: "not good!",
+				},
+			],
+			likedBy: [
+				{
+					_id: "u108",
+					fullname: "Ted Rice",
+					imgUrl: "https://randomuser.me/api/portraits/men/56.jpg",
+				},
+				{
+					_id: "u106",
+					fullname: "Dob",
+					imgUrl: "http://some-img",
+				},
+			],
+			tags: ["fun", "romantic"],
+		},
+		{
+			_id: "s103",
+			txt: "Beautiful sunrise!!!",
+			imgUrl:
+				"https://images.unsplash.com/photo-1495616811223-4d98c6e9c869?q=80&w=468&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+			by: {
+				_id: "Xma2l",
+				fullname: "Koko Jambo",
+				imgUrl: "https://randomuser.me/api/portraits/women/16.jpg",
+			},
+			loc: {
+				// Optional
+				lat: 11.11,
+				lng: 22.22,
+				name: "Tel Aviv",
+			},
+			comments: [
+				{
+					id: "c1001",
+					by: {
+						_id: "u105",
+						fullname: "Bob",
+						imgUrl: "http://some-img",
+					},
+					txt: "good one!",
+					likedBy: [
+						// Optional
+						{
+							_id: "u105",
+							fullname: "Bob",
+							imgUrl: "http://some-img",
+						},
+					],
+				},
+				{
+					id: "c1002",
+					by: {
+						_id: "u106",
+						fullname: "Dob",
+						imgUrl: "http://some-img",
+					},
+					txt: "not good!",
+				},
+			],
+			likedBy: [
+				{
+					_id: "u105",
+					fullname: "Bob",
+					imgUrl: "http://some-img",
+				},
+				{
+					_id: "u106",
+					fullname: "Dob",
+					imgUrl: "http://some-img",
+				},
+			],
+			tags: ["fun", "romantic"],
+		},
+		{
+			_id: "s104",
+			txt: "Best trip ever",
+			imgUrl:
+				"https://cdn.pixabay.com/photo/2023/01/04/13/21/animals-7696695_640.jpg",
+			by: {
+				_id: "u101",
+				fullname: "Ulash Ulashi",
+				imgUrl: "http://some-img",
+			},
+			loc: {
+				// Optional
+				lat: 11.11,
+				lng: 22.22,
+				name: "Tel Aviv",
+			},
+			comments: [
+				{
+					id: "c1001",
+					by: {
+						_id: "u105",
+						fullname: "Bob",
+						imgUrl: "http://some-img",
+					},
+					txt: "good one!",
+					likedBy: [
+						// Optional
+						{
+							_id: "u105",
+							fullname: "Bob",
+							imgUrl: "http://some-img",
+						},
+					],
+				},
+				{
+					id: "c1002",
+					by: {
+						_id: "u106",
+						fullname: "Dob",
+						imgUrl: "http://some-img",
+					},
+					txt: "not good!",
+				},
+			],
+			likedBy: [
+				{
+					_id: "u105",
+					fullname: "Bob",
+					imgUrl: "http://some-img",
+				},
+				{
+					_id: "u106",
+					fullname: "Dob",
+					imgUrl: "http://some-img",
+				},
+			],
+			tags: ["fun", "romantic"],
+		},
+		{
+			_id: "s105",
+			txt: "Best trip ever",
+			imgUrl:
+				"https://cdn.pixabay.com/photo/2023/01/04/13/21/animals-7696695_640.jpg",
+			by: {
+				_id: "u101",
+				fullname: "Ulash Ulashi",
+				imgUrl: "http://some-img",
+			},
+			loc: {
+				// Optional
+				lat: 11.11,
+				lng: 22.22,
+				name: "Tel Aviv",
+			},
+			comments: [
+				{
+					id: "c1001",
+					by: {
+						_id: "u105",
+						fullname: "Bob",
+						imgUrl: "http://some-img",
+					},
+					txt: "good one!",
+					likedBy: [
+						// Optional
+						{
+							_id: "u105",
+							fullname: "Bob",
+							imgUrl: "http://some-img",
+						},
+					],
+				},
+				{
+					id: "c1002",
+					by: {
+						_id: "u106",
+						fullname: "Dob",
+						imgUrl: "http://some-img",
+					},
+					txt: "not good!",
+				},
+			],
+			likedBy: [
+				{
+					_id: "u105",
+					fullname: "Bob",
+					imgUrl: "http://some-img",
+				},
+				{
+					_id: "u106",
+					fullname: "Dob",
+					imgUrl: "http://some-img",
+				},
+			],
+			tags: ["fun", "romantic"],
+		},
+		{
+			_id: "s106",
+			txt: "off we goooooooooo...",
+			imgUrl:
+				"https://images.unsplash.com/photo-1527936599657-e6d24be0c95c?q=80&w=468&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+			by: {
+				_id: "Xma2l",
+				fullname: "Koko Jambo",
+				imgUrl: "https://randomuser.me/api/portraits/women/16.jpg",
+			},
+			loc: {
+				// Optional
+				lat: 11.11,
+				lng: 22.22,
+				name: "Tel Aviv",
+			},
+			comments: [
+				{
+					id: "c1001",
+					by: {
+						_id: "u104",
+						fullname: "Bob",
+						imgUrl: "https://randomuser.me/api/portraits/men/32.jpg",
+					},
+					txt: "good one!",
+					likedBy: [
+						// Optional
+						{
+							_id: "u107",
+							fullname: "Joyce Lane",
+							imgUrl: "https://randomuser.me/api/portraits/women/3.jpg",
+						},
+					],
+				},
+				{
+					id: "c1002",
+					by: {
+						_id: "u106",
+						fullname: "Max Steward",
+						imgUrl: "https://randomuser.me/api/portraits/men/54.jpg",
+					},
+					txt: "not good!",
+				},
+				{
+					id: "c1003",
+					by: {
+						_id: "u110",
+						fullname: "Lydia Brewer",
+						imgUrl: "https://randomuser.me/api/portraits/woman/56.jpg",
+					},
+					txt: "Have a nice trip!",
+					likedBy: [
+						// Optional
+						{
+							_id: "u107",
+							fullname: "Joyce Lane",
+							imgUrl: "https://randomuser.me/api/portraits/women/3.jpg",
+						},
+					],
+				},
+			],
+			likedBy: [
+				{
+					_id: "u108",
+					fullname: "Ted Rice",
+					imgUrl: "https://randomuser.me/api/portraits/men/56.jpg",
+				},
+				{
+					_id: "u106",
+					fullname: "Max Steward",
+					imgUrl: "https://randomuser.me/api/portraits/men/54.jpg",
+				},
+			],
+			tags: ["fun", "romantic"],
+		},
+		{
+			_id: "s590",
+			txt: "magnificent...",
+			imgUrl:
+				"https://plus.unsplash.com/premium_photo-1694475347672-560fa2f92fde?q=80&w=468&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+			by: {
+				_id: "u107",
+				fullname: "Joyce Lane",
+				imgUrl: "https://randomuser.me/api/portraits/women/3.jpg",
+			},
+			loc: {
+				// Optional
+				lat: 11.11,
+				lng: 22.22,
+				name: "Tel Aviv",
+			},
+			comments: [
+				{
+					id: "c1001",
+					by: {
+						_id: "u104",
+						fullname: "Bob",
+						imgUrl: "https://randomuser.me/api/portraits/men/32.jpg",
+					},
+					txt: "good one!",
+					likedBy: [
+						// Optional
+					],
+				},
+				{
+					id: "c1002",
+					by: {
+						_id: "u106",
+						fullname: "Max Steward",
+						imgUrl: "https://randomuser.me/api/portraits/men/54.jpg",
+					},
+					txt: "not good!",
+				},
+				{
+					id: "c1003",
+					by: {
+						_id: "u110",
+						fullname: "Lydia Brewer",
+						imgUrl: "https://randomuser.me/api/portraits/woman/56.jpg",
+					},
+					txt: "Have a nice trip!",
+					likedBy: [
+						// Optional
+						{
+							_id: "u107",
+							fullname: "Joyce Lane",
+							imgUrl: "https://randomuser.me/api/portraits/women/3.jpg",
+						},
+					],
+				},
+			],
+			likedBy: [
+				{
+					_id: "u108",
+					fullname: "Ted Rice",
+					imgUrl: "https://randomuser.me/api/portraits/men/56.jpg",
+				},
+				{
+					_id: "u106",
+					fullname: "Max Steward",
+					imgUrl: "https://randomuser.me/api/portraits/men/54.jpg",
+				},
+			],
+			tags: ["fun", "romantic"],
+		},
+		{
+			_id: "2134",
+			txt: "lovely...",
+			imgUrl:
+				"https://plus.unsplash.com/premium_photo-1694475350267-3d15915d97af?q=80&w=468&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+			by: {
+				_id: "u111",
+				fullname: "Bernice Fox",
+				imgUrl: "https://randomuser.me/api/portraits/women/19.jpg",
+			},
+			loc: {
+				// Optional
+				lat: 11.11,
+				lng: 22.22,
+				name: "Tel Aviv",
+			},
+			comments: [
+				{
+					id: "c1001",
+					by: {
+						_id: "u104",
+						fullname: "Bob",
+						imgUrl: "https://randomuser.me/api/portraits/men/32.jpg",
+					},
+					txt: "good one!",
+					likedBy: [
+						// Optional
+						{
+							_id: "u107",
+							fullname: "Joyce Lane",
+							imgUrl: "https://randomuser.me/api/portraits/women/3.jpg",
+						},
+					],
+				},
+				{
+					id: "c1002",
+					by: {
+						_id: "u106",
+						fullname: "Max Steward",
+						imgUrl: "https://randomuser.me/api/portraits/men/54.jpg",
+					},
+					txt: "not good!",
+				},
+				{
+					id: "c1003",
+					by: {
+						_id: "u110",
+						fullname: "Lydia Brewer",
+						imgUrl: "https://randomuser.me/api/portraits/woman/56.jpg",
+					},
+					txt: "Have a nice trip!",
+					likedBy: [
+						// Optional
+						{
+							_id: "u107",
+							fullname: "Joyce Lane",
+							imgUrl: "https://randomuser.me/api/portraits/women/3.jpg",
+						},
+					],
+				},
+			],
+			likedBy: [
+				{
+					_id: "u108",
+					fullname: "Ted Rice",
+					imgUrl: "https://randomuser.me/api/portraits/men/56.jpg",
+				},
+				{
+					_id: "u106",
+					fullname: "Max Steward",
+					imgUrl: "https://randomuser.me/api/portraits/men/54.jpg",
+				},
+			],
+			tags: ["fun", "romantic"],
+		},
+		{
+			_id: "s245",
+			txt: "I have a dream...",
+			imgUrl:
+				"https://images.unsplash.com/photo-1740309056732-d8d5f318b216?q=80&w=468&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+			by: {
+				_id: "u111",
+				fullname: "Bernice Fox",
+				imgUrl: "https://randomuser.me/api/portraits/women/19.jpg",
+			},
+			loc: {
+				// Optional
+				lat: 11.11,
+				lng: 22.22,
+				name: "Tel Aviv",
+			},
+			comments: [
+				{
+					id: "c1001",
+					by: {
+						_id: "u104",
+						fullname: "Bob",
+						imgUrl: "https://randomuser.me/api/portraits/men/32.jpg",
+					},
+					txt: "good one!",
+					likedBy: [
+						// Optional
+						{
+							_id: "u107",
+							fullname: "Joyce Lane",
+							imgUrl: "https://randomuser.me/api/portraits/women/3.jpg",
+						},
+					],
+				},
+				{
+					id: "c1002",
+					by: {
+						_id: "u106",
+						fullname: "Max Steward",
+						imgUrl: "https://randomuser.me/api/portraits/men/54.jpg",
+					},
+					txt: "👍",
+				},
+				{
+					id: "c1003",
+					by: {
+						_id: "u110",
+						fullname: "Lydia Brewer",
+						imgUrl: "https://randomuser.me/api/portraits/woman/56.jpg",
+					},
+					txt: "Have a nice trip!",
+					likedBy: [
+						// Optional
+						{
+							_id: "u107",
+							fullname: "Joyce Lane",
+							imgUrl: "https://randomuser.me/api/portraits/women/3.jpg",
+						},
+					],
+				},
+			],
+			likedBy: [
+				{
+					_id: "u108",
+					fullname: "Ted Rice",
+					imgUrl: "https://randomuser.me/api/portraits/men/56.jpg",
+				},
+				{
+					_id: "u106",
+					fullname: "Max Steward",
+					imgUrl: "https://randomuser.me/api/portraits/men/54.jpg",
+				},
+			],
+			tags: ["fun", "romantic"],
+		},
+	];
 
 	// storageService._save(STORAGE_KEY, story)
 	utilService.saveToStorage(STORAGE_KEY, story);
